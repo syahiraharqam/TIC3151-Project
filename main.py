f11d7f5a19f76a3384d6275dc44a25d4bf1fc93a
Syahirah <syahiraharqam@gmail.com>
2022-04-04 10:39:32 +0800

    np

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import preprocessing
-
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
